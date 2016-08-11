eaea210f13cdb0261f7eeabe85a363dd3ddaca4c
ekeast <ekeast328@gmail.com>
2016-08-11 17:45:19 -0400

    API

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,18 @@ Rails.application.routes.draw do
     post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
 end
